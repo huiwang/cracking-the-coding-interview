07636baef83839afc1be1d4a59cf9384d3d05e8f
Hui Wang <hedonplay@gmail.com>
2017-11-09 14:27:49 +0100

    ˆ

--- src/main/java/com/truelaure/ctci/bitmanipulation/_05_01_Insertion.java
+++ src/main/java/com/truelaure/ctci/bitmanipulation/_05_01_Insertion.java
@@ -16,7 +16,7 @@ class _05_01_Insertion {
     int insert(int n, int m, int i, int j) {
         int allOnes = -1;
         int left = allOnes << (j + 1);
-        int right = ~(allOnes << i);
+        int right = (1 << i) - 1;
         int mask = left | right;
         int shiftedM = m << i;
         int clearedN = n & mask;
