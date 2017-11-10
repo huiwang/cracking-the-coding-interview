7b0ac18716da84bcd8d15f38c296f4d9591c8a39
Hui Wang <hedonplay@gmail.com>
2017-11-10 11:31:48 +0100

    improve debugger comment

--- src/main/java/com/truelaurel/ctci/bitmanipulation/_05_05_Debugger.java
+++ src/main/java/com/truelaurel/ctci/bitmanipulation/_05_05_Debugger.java
@@ -5,7 +5,7 @@ package com.truelaurel.ctci.bitmanipulation;
  */
 public class _05_05_Debugger {
 
-    // n & (n - 1) clears the last bit 1, if nothing remains. it means there is only is one 1.
+    // n & (n - 1) clears the least significant bit, if nothing remains. it means there is only is one 1.
     // therefore it's a power of 2
     boolean debug(int n) {
         return (n & (n - 1)) == 0;
