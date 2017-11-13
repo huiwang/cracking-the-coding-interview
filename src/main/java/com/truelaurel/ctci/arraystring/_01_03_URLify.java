9c4b25e99282d97e57ced9cf6642c4104e2dfe8f
Hui Wang <hedonplay@gmail.com>
2017-11-13 21:05:54 +0100

    add description

--- src/main/java/com/truelaurel/ctci/arraystring/_01_03_URLify.java
+++ src/main/java/com/truelaurel/ctci/arraystring/_01_03_URLify.java
@@ -1,5 +1,15 @@
 package com.truelaurel.ctci.arraystring;
 
+/**
+ * Write a method to replace all spaces in a string with '%20'
+ * You may assume that the string has sufficient space at the end to hold the additional characters,
+ * and that you are given the "true" length of the string.
+ * (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
+ * <p>
+ * EXAMPLE
+ * Input:  "Mr John Smith    ", 13
+ * Output: "Mr%20John%20Smith"
+ */
 class _01_03_URLify {
     char[] urlify(char[] chars, int trueLength) {
         int spaces = 0;
