9b63157055e3c57b5ba9c504bed8d810fcdfa87f
Hui Wang <hedonplay@gmail.com>
2017-11-27 22:19:12 +0100

    add graph

--- src/test/java/com/truelaurel/ctci/treegraph/_04_07_BuildOrderTest.java
+++ src/test/java/com/truelaurel/ctci/treegraph/_04_07_BuildOrderTest.java
@@ -35,6 +35,14 @@ public class _04_07_BuildOrderTest {
     }
 
     @Test
+    public void withTwoGroups() {
+        Digraph digraph = new Digraph(4);
+        digraph.addEdge(1, 0);
+        digraph.addEdge(3, 2);
+        assertEquals(Arrays.asList(0, 1, 2, 3), s.findBuildOrder(digraph));
+    }
+
+    @Test
     public void withCycle() {
         Digraph digraph = new Digraph(3);
         digraph.addEdge(0, 1);
