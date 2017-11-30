471fd240aa2ae09d41c89608b2e362a0496e245e
Hui Wang <hedonplay@gmail.com>
2017-11-30 20:54:06 +0100

    path with sum first solution

---
+++ src/test/java/com/truelaurel/ctci/treegraph/_04_12_PathsWithSumTest.java
@@ -0,0 +1,28 @@
+package com.truelaurel.ctci.treegraph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _04_12_PathsWithSumTest {
+
+    private _04_12_PathsWithSum s = new _04_12_PathsWithSum();
+
+    @Test
+    public void withOneNode() {
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        assertEquals(0, s.countPathWithSum(root, 2));
+        assertEquals(1, s.countPathWithSum(root, 1));
+    }
+
+    @Test
+    public void withThreeNodes() {
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        root.left = new BinaryTreeNode(1);
+        root.right = new BinaryTreeNode(2);
+        assertEquals(2, s.countPathWithSum(root, 1));
+        assertEquals(2, s.countPathWithSum(root, 2));
+        assertEquals(1, s.countPathWithSum(root, 3));
+        assertEquals(0, s.countPathWithSum(root, 4));
+    }
+}
