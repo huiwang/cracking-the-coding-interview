c578af99a0da42f371805b25eda867160cc0a984
Hui Wang <hedonplay@gmail.com>
2017-12-04 23:29:50 +0100

    add running sum solution

--- src/main/java/info/hui_wang/ctci/treegraph/_04_12_PathsWithSum.java
+++ src/main/java/info/hui_wang/ctci/treegraph/_04_12_PathsWithSum.java
@@ -1,29 +1,37 @@
 package info.hui_wang.ctci.treegraph;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * You are given a binary tree in which each node contains an integer value (which might be positive or negative).
- * Design an algorithm to count the number of paths that sum to a given value.
+ * Design an algorithm to count the number of paths that target to a given value.
  * The path does not need to start or end at the root or a leaf, but it must go downwards
  * (traveling only from parent nodes to child nodes).
  */
 class _04_12_PathsWithSum {
 
 
+    private int target;
+    private Map<Integer, Integer> runningSumToCount;
+    private int count;
+
     int countPathWithSum(BinaryTreeNode node, int sum) {
-        return countHelper(node, sum, sum);
+        count = 0;
+        runningSumToCount = new HashMap<>();
+        runningSumToCount.put(0, 1);
+        target = sum;
+        countHelper(node, 0);
+        return count;
     }
 
-    private int countHelper(BinaryTreeNode node, int sum, int original) {
-        if (node == null) return 0;
-        int count = 0;
-        if (node.val == sum) {
-            count += 1;
-        } else {
-            count += countHelper(node.left, sum - node.val, original)
-                    + countHelper(node.right, sum - node.val, original);
-        }
-        count += countPathWithSum(node.left, original);
-        count += countPathWithSum(node.right, original);
-        return count;
+    private void countHelper(BinaryTreeNode node, int acc) {
+        if (node == null) return;
+        int runningSum = acc + node.val;
+        runningSumToCount.merge(runningSum, 1, Math::addExact);
+        count += runningSumToCount.getOrDefault(runningSum - target, 0);
+        countHelper(node.left, runningSum);
+        countHelper(node.right, runningSum);
+        runningSumToCount.computeIfPresent(runningSum, (k, v) -> v == 1 ? null : v - 1);
     }
 }
