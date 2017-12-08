5cde9636cb39b5bab6387a3140a3097bacf1ff32
Hui Wang <hedonplay@gmail.com>
2017-12-08 22:14:23 +0100

    add robot

---
+++ src/main/java/recursivedp/_08_02_RobotInAGrid.java
@@ -0,0 +1,19 @@
+package recursivedp;
+
+import java.util.List;
+
+/**
+ * Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+ * The robot can only move in two directions, right and down, but certain cells are "off limits"
+ * such that the robot cannot step on them. Design an algorithm to find a path for the robot
+ * from the top left to the bottom right.
+ */
+class _08_02_RobotInAGrid {
+    List<Direction> findPath(boolean[][] grid) {
+        throw new UnsupportedOperationException();
+    }
+
+    enum Direction {
+        RIGHT, DOWN
+    }
+}
---
+++ src/test/java/recursivedp/_08_02_RobotInAGridTest.java
@@ -0,0 +1,19 @@
+package recursivedp;
+
+import org.junit.Test;
+
+public class _08_02_RobotInAGridTest {
+
+
+    private _08_02_RobotInAGrid s = new _08_02_RobotInAGrid();
+
+    @Test
+    public void test() {
+        boolean[][] grid = new boolean[][]{
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+
+    }
+}
