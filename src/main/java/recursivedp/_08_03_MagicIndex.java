78cd41c74824639e8cbf713b7eedbfb599539d1a
Hui Wang <hedonplay@gmail.com>
2017-12-13 20:07:47 +0100

    add more questions

--- src/main/java/recursivedp/_08_03_MagicIndex.java
+++ src/main/java/recursivedp/_08_03_MagicIndex.java
@@ -7,22 +7,6 @@ package recursivedp;
 class _08_03_MagicIndex {
 
     int findMagicIndex(int[] arr) {
-        if (arr == null || arr.length == 0) {
-            return -1;
-        }
-
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] == mid) {
-                return mid;
-            } else if (arr[mid] > mid) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return -1;
+        throw new UnsupportedOperationException();
     }
 }
--- src/main/java/recursivedp/_08_04_PowerSet.java
+++ src/main/java/recursivedp/_08_04_PowerSet.java
@@ -1,9 +1,6 @@
 package recursivedp;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Write a method to return all subsets of a set
@@ -11,24 +8,8 @@ import java.util.stream.Collectors;
 class _08_04_PowerSet {
 
     Set<Set<Integer>> subSets(Set<Integer> set) {
-        if (set.isEmpty()) return Collections.emptySet();
-        Set<Set<Integer>> subsets = new HashSet<>();
-        subsets.add(new HashSet<>());
-        for (Integer integer : set) {
-            subsets = grow(subsets, integer);
-        }
-        return subsets;
+        throw new UnsupportedOperationException();
     }
 
-    private Set<Set<Integer>> grow(Set<Set<Integer>> subsets, Integer i) {
-        Set<Set<Integer>> grown = subsets.stream().map(set -> grow(i, set)).collect(Collectors.toSet());
-        grown.addAll(subsets);
-        return grown;
-    }
 
-    private HashSet<Integer> grow(Integer i, Set<Integer> set) {
-        HashSet<Integer> newSet = new HashSet<>(set);
-        newSet.add(i);
-        return newSet;
-    }
 }
