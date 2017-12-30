aea7dee15c6368fdb9fa06abfe0f4544e6038147
Hui Wang <hedonplay@gmail.com>
2017-12-30 09:55:30 +0100

    add permutation

--- src/main/java/recursivedp/_08_07_PermutationWithoutDup.java
+++ src/main/java/recursivedp/_08_07_PermutationWithoutDup.java
@@ -1,7 +1,5 @@
 package recursivedp;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,18 +8,7 @@ import java.util.Set;
 class _08_07_PermutationWithoutDup {
 
     Set<String> computePermutation(String str) {
-        if (str.length() == 0) return Collections.singleton("");
-        Set<String> result = new HashSet<>();
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            String prefix = str.substring(0, i);
-            String remainder = str.substring(i + 1, str.length());
-            Set<String> partial = computePermutation(prefix + remainder);
-            for (String s : partial) {
-                result.add(c + s);
-            }
-        }
-        return result;
+        throw new UnsupportedOperationException();
     }
 
 }
--- src/main/java/recursivedp/_08_08_PermutationWithDup.java
+++ src/main/java/recursivedp/_08_08_PermutationWithDup.java
@@ -1,6 +1,6 @@
 package recursivedp;
 
-import java.util.*;
+import java.util.Set;
 
 /**
  * Write a method to compute all permutations of a string whose characters
@@ -9,35 +9,7 @@ import java.util.*;
 class _08_08_PermutationWithDup {
 
     Set<String> computePermutation(String str) {
-        if (str.length() == 0) return Collections.singleton("");
-        Map<Character, Integer> frequencyTable = buildFrequencyTable(str);
-        Set<String> result = new HashSet<>();
-        dfs(frequencyTable, "", str.length(), result);
-        return result;
+        throw new UnsupportedOperationException();
     }
-
-    private void dfs(Map<Character, Integer> map, String prefix, int remaining, Set<String> result) {
-        if (remaining == 0) {
-            result.add(prefix);
-        } else {
-            for (Character c : map.keySet()) {
-                int count = map.get(c);
-                if (count > 0) {
-                    map.put(c, count - 1);
-                    dfs(map, prefix + c, remaining - 1, result);
-                    map.put(c, count);
-                }
-            }
-        }
-    }
-
-    private Map<Character, Integer> buildFrequencyTable(String str) {
-        Map<Character, Integer> m = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            m.compute(str.charAt(i), (k, v) -> v == null ? 1 : v + 1);
-        }
-        return m;
-    }
-
 
 }
