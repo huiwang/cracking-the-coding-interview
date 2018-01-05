38f4472beac7ad649c09fb4356efe58d98418622
Hui Wang <hedonplay@gmail.com>
2018-01-05 21:42:08 +0100

    add parens question

--- src/main/java/recursivedp/_08_09_Parens.java
+++ src/main/java/recursivedp/_08_09_Parens.java
@@ -1,7 +1,5 @@
 package recursivedp;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -12,27 +10,8 @@ import java.util.Set;
  */
 public class _08_09_Parens {
 
-    //time o(2^n) space o(n) where n = pairs * 2
     Set<String> combine(int pairs) {
-        if (pairs == 0) return Collections.emptySet();
-        Set<String> result = new HashSet<>();
-        char[] buff = new char[pairs * 2];
-        doCombine(pairs, 0, 0, result, buff);
-        return result;
+        throw new UnsupportedOperationException();
     }
 
-    private void doCombine(int pairs, int open, int close, Set<String> result, char[] buff) {
-        if (open == pairs && close == pairs) {
-            result.add(String.valueOf(buff));
-        } else {
-            if (open < pairs) {
-                buff[open + close] = '(';
-                doCombine(pairs, open + 1, close, result, buff);
-            }
-            if (close < open) {
-                buff[open + close] = ')';
-                doCombine(pairs, open, close + 1, result, buff);
-            }
-        }
-    }
 }
