17d6467a11358e21cbc69855f4c5ff230d6b5935
Hui Wang <hedonplay@gmail.com>
2018-01-10 22:27:20 +0100

    add eight queens

--- src/main/java/recursivedp/_08_12_EightQueens.java
+++ src/main/java/recursivedp/_08_12_EightQueens.java
@@ -1,8 +1,6 @@
 package recursivedp;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,69 +10,10 @@ import java.util.List;
  */
 public class _08_12_EightQueens {
 
-    private static final int SIZE = 8;
 
     List<List<Point>> arrange() {
-        List<List<Point>> result = new ArrayList<>();
-        Constraints constraints = new Constraints();
-        dfs(0, new Point[SIZE], constraints, result);
-        return result;
+        throw new UnsupportedOperationException();
     }
 
-    private void dfs(int row, Point[] points, Constraints constraints, List<List<Point>> result) {
-        if (row == SIZE) {
-            result.add(Arrays.asList(points));
-            return;
-        }
-
-        for (int col = 0; col < SIZE; col++) {
-            Point p = new Point(row, col);
-            if (constraints.isFeasible(p)) {
-                constraints.mark(p);
-                points[row] = p;
-                dfs(row + 1, points, constraints, result);
-                constraints.unmark(p);
-            }
-        }
-
-    }
-
-    private static class Constraints {
-        private boolean[] rows = new boolean[SIZE];
-        private boolean[] cols = new boolean[SIZE];
-        private boolean[] positive = new boolean[SIZE * 2 - 1];
-        private boolean[] negative = new boolean[SIZE * 2 - 1];
-
-        private void mark(Point p) {
-            rows[p.x] = true;
-            cols[p.y] = true;
-            positive[toPositiveDiagonal(p)] = true;
-            negative[toNegativeDiagonal(p)] = true;
-        }
-
-        private void unmark(Point p) {
-            rows[p.x] = false;
-            cols[p.y] = false;
-            positive[toPositiveDiagonal(p)] = false;
-            negative[toNegativeDiagonal(p)] = false;
-        }
-
-        private boolean isFeasible(Point p) {
-            return !rows[p.x]
-                    && !cols[p.y]
-                    && !positive[toPositiveDiagonal(p)]
-                    && !negative[toNegativeDiagonal(p)];
-        }
-
-        private int toPositiveDiagonal(Point p) {
-            return p.x + p.y;
-        }
-
-
-        private int toNegativeDiagonal(Point p) {
-            return SIZE - 1 - (p.x - p.y);
-        }
-
-    }
 
 }
