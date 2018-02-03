114e36c696d0a7b331ec0b98b5a6dcbbe78c3b6d
Hui Wang <hedonplay@gmail.com>
2018-02-03 22:17:00 +0100

    group anagram

---
+++ src/main/java/sortingsearching/_10_02_GroupAnagrams.java
@@ -0,0 +1,25 @@
+package sortingsearching;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Write a method to sort an array of strings so that all the anagrams are next to each other.
+ */
+public class _10_02_GroupAnagrams {
+
+    List<String> groupAnagrams(List<String> words) {
+        if (words == null || words.isEmpty()) return words;
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for (String word : words) {
+            anagrams.computeIfAbsent(sorted(word), k -> new ArrayList<>()).add(word);
+        }
+        return anagrams.values().stream().flatMap(List::stream).collect(Collectors.toList());
+    }
+
+    private String sorted(String word) {
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return String.valueOf(chars);
+    }
+}
