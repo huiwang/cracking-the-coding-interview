a707cca35f80195362900ed400613d4ee63f7b6e
Hui Wang <hedonplay@gmail.com>
2018-02-03 21:25:11 +0100

    add sorted merge

--- src/main/java/sortingsearching/_10_01_SortedMerge.java
+++ src/main/java/sortingsearching/_10_01_SortedMerge.java
@@ -7,22 +7,6 @@ package sortingsearching;
 public class _10_01_SortedMerge {
 
     int[] merge(int[] big, int[] small) {
-        int b = big.length - small.length - 1;
-        int s = small.length - 1;
-        int i = big.length - 1;
-        while (b >= 0 || s >= 0) {
-            if (b < 0) {
-                big[i--] = small[s--];
-            } else if (s < 0) {
-                break;
-            } else {
-                if (small[s] > big[b]) {
-                    big[i--] = small[s--];
-                } else {
-                    big[i--] = big[b--];
-                }
-            }
-        }
-        return big;
+        throw new UnsupportedOperationException();
     }
 }
