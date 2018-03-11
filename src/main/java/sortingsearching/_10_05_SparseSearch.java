23003d7adc627b81432edfa2897ac6566eb1bff9
Hui Wang <hedonplay@gmail.com>
2018-03-11 10:26:32 +0100

    add sparse search

--- src/main/java/sortingsearching/_10_05_SparseSearch.java
+++ src/main/java/sortingsearching/_10_05_SparseSearch.java
@@ -1,7 +1,5 @@
 package sortingsearching;
 
-import java.util.Objects;
-
 /**
  * Given a sorted array of strings that is interspersed with empty strings,
  * write a method to find the location of a given string.
@@ -9,42 +7,9 @@ import java.util.Objects;
 public class _10_05_SparseSearch {
 
     int find(String[] arr, String target) {
-        if (target == null || Objects.equals("", target)) return -1;
-        return doFind(arr, target, 0, arr.length - 1);
+        throw new UnsupportedOperationException();
     }
 
-    private int doFind(String[] arr, String target, int start, int end) {
-        if (start > end) return -1;
-        int mid = start + (end - start) / 2;
-
-
-        if (arr[mid].isEmpty()) {
-            int left = mid;
-            int right = mid;
-            while (true) {
-                if (left < start && right > end) return -1;
-                if (!arr[left].isEmpty()) {
-                    mid = left;
-                    break;
-                }
-
-                if (!arr[right].isEmpty()) {
-                    mid = right;
-                    break;
-                }
-                left--;
-                right++;
-            }
-        }
-
-        if (arr[mid].equals(target)) {
-            return mid;
-        } else if (target.compareTo(arr[mid]) < 0) {
-            return doFind(arr, target, start, mid - 1);
-        } else {
-            return doFind(arr, target, mid + 1, end);
-        }
-    }
 
 
 }
