e98d5cc84204071dabc046ed99abd5bf11a1b7f0
Hui Wang <hedonplay@gmail.com>
2018-03-11 10:24:46 +0100

    add sparse search

---
+++ src/test/java/sortingsearching/_10_05_SparseSearchTest.java
@@ -0,0 +1,31 @@
+package sortingsearching;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class _10_05_SparseSearchTest {
+
+    private final _10_05_SparseSearch sparseSearch = new _10_05_SparseSearch();
+
+    @Test
+    public void withEmptyString() {
+        Assert.assertEquals(-1, sparseSearch.find(new String[]{"hello", "", "world"}, ""));
+    }
+
+    @Test
+    public void withValidString() {
+        Assert.assertEquals(8, sparseSearch.find(
+                new String[]{"hello", "", "", "idea", "", "", "", "", "world", "", "", "", "", "", "xylo", "", ""},
+                "world"));
+
+    }
+
+
+    @Test
+    public void withManySparse() {
+        Assert.assertEquals(0, sparseSearch.find(
+                new String[]{"hello", "", "", "", "", "", "", "", "", "", "", "", "", ""},
+                "hello"));
+
+    }
+}
