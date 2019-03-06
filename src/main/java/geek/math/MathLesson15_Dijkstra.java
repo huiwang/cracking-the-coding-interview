ab6b172272421af9f7d1dc1da81f6180b75c5252
thomasq0 <thomasqiu7@gmail.com>
2019-03-06 14:41:24 +1100

    complete dijkstra

--- src/main/java/geek/math/MathLesson15_Dijkstra.java
+++ src/main/java/geek/math/MathLesson15_Dijkstra.java
@@ -1,18 +1,101 @@
 package geek.math;
 
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MathLesson15_Dijkstra {
 
+    int size = 0;
     DijkstraVertex[] vertices;
 
-    public double getBestPaTH(int a, int b) {
-        return 0;
+    public MathLesson15_Dijkstra(int size) {
+        this.size = size;
+        vertices = new DijkstraVertex[size];
+    }
+
+    public double getBestPath(int a, int b) {
+        if (a < 0 || b < 0 || a >= vertices.length || b >= vertices.length) {
+            throw new IllegalArgumentException("source or target out of range");
+        }
+        if (vertices[a] == null || vertices[b] == null) {
+            throw new IllegalArgumentException("source or target does not exist");
+        }
+        if (a == b) {
+            System.out.println("The shortest path from " + a + " to " + b + ": 0");
+            return 0.0;
+        }
+        Set<DijkstraVertex> set = new HashSet<>();
+        Map<Integer, Double> dist = new HashMap<>();
+        Map<Integer, Integer> prev = new HashMap<>();
+        for (DijkstraVertex v : vertices) {
+            dist.put(v.id, Double.POSITIVE_INFINITY);
+            prev.put(v.id, Integer.MAX_VALUE);
+            set.add(v);
+        }
+        dist.put(a, 0.0);
+        while (!set.isEmpty()) {
+            DijkstraVertex shortest = getShortestVertex(set, dist);
+            if (shortest == null) {
+                // cannot find connected vertex
+                break;
+            }
+            set.remove(shortest);
+            Integer id = shortest.id;
+            Double p = dist.get(id);
+            if (p == Double.POSITIVE_INFINITY) {
+                break;
+            }
+            if (shortest.connections != null) {
+                shortest.connections.forEach((k,v) -> {
+                    Double distance = p + v;
+                    if (distance < dist.get(k)) {
+                        dist.put(k, distance);
+                        prev.put(k, id);
+                    }
+                });
+            }
+        }
+        System.out.println("The shortest path from " + a + " to " + b + ":");
+        if (dist.get(b) != Double.POSITIVE_INFINITY) {
+            Double result =  dist.get(b);
+            List<DijkstraVertex> path = getPath(a, b, prev);
+            path.forEach(v -> System.out.print(String.format("%d(%.2f) ", v.id, dist.get(v.id))));
+            System.out.print("\n");
+            return result;
+        } else {
+            System.out.println("None");
+            return Double.NEGATIVE_INFINITY;
+        }
+    }
+
+    private List<DijkstraVertex> getPath(int a, int b, Map<Integer, Integer> prev) {
+        // assume a and b are valid here as it is a private method
+        List<DijkstraVertex> path = new ArrayList<>();
+        int curr = b;
+        while (curr != Integer.MAX_VALUE) {
+            path.add(vertices[curr]);
+            curr = prev.get(curr);
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    private static DijkstraVertex getShortestVertex(Collection<DijkstraVertex> c, Map<Integer, Double> dist) {
+        if (c == null) {
+            return null;
+        }
+        Double min = Double.POSITIVE_INFINITY;
+        DijkstraVertex chosen = null;
+        for (DijkstraVertex v : c) {
+            Double d = dist.get(v.id);
+            if (d < min) {
+                chosen = v;
+                min = d;
+            }
+        }
+        return chosen;
     }
 }
 
@@ -35,8 +118,8 @@ class DijkstraVertex {
         this.connections = connections;
     }
 
-    private static Pattern pattern = Pattern.compile("^(\\\\d+)\\\\s*(\\\\(.*\\\\))?$");
-    private static Pattern connection_pattern = Pattern.compile("(\\\\(\\\\s*(\\\\d+)\\\\s*,\\\\s*([0-9]*[.]?[0-9]+)\\\\s*\\\\)){1,}");
+    private static Pattern pattern = Pattern.compile("^(\\d+)\\s*(\\(.*\\))?$");
+    private static Pattern connection_pattern = Pattern.compile("\\s*(\\(\\s*(\\d+)\\s*,\\s*([0-9]*[.]?[0-9]+)\\s*\\))\\s*");
 
     public static DijkstraVertex parse(String content) {
         Matcher matcher = pattern.matcher(content);
@@ -52,7 +135,23 @@ class DijkstraVertex {
                     v.addConnection(vid, w);
                 }
             }
+            return v;
+        } else {
+            return null;
         }
-        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DijkstraVertex that = (DijkstraVertex) o;
+        return id == that.id &&
+                Objects.equals(connections, that.connections);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, connections);
     }
 }
--- src/test/java/geek/math/MathLesson15_DijkstraTest.java
+++ src/test/java/geek/math/MathLesson15_DijkstraTest.java
@@ -3,15 +3,81 @@ package geek.math;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class MathLesson15_DijkstraTest {
 
+    MathLesson15_Dijkstra dijkstra;
+
     @Before
     public void setUp() throws Exception {
+        dijkstra = new MathLesson15_Dijkstra(10);
+        /**
+         * append(new Node(0, new int[]{1, 8}))
+         *                 .append(new Node(1, new int[]{0, 6, 7, 9}))
+         *                 .append(new Node(2, new int[]{4, 9}))
+         *                 .append(new Node(3, new int[]{5}))
+         *                 .append(new Node(4, new int[]{2, 6, 7}))
+         *                 .append(new Node(5, new int[]{3}))
+         *                 .append(new Node(6, new int[]{1, 4}))
+         *                 .append(new Node(7, new int[]{1, 4, 8, 9}))
+         *                 .append(new Node(8, new int[]{0, 7}))
+         *                 .append(new Node(9, new int[]{1, 2, 7}));
+         */
+        dijkstra.vertices[0] = DijkstraVertex.parse("0(1, 0.3)(8, 0.1)");
+        dijkstra.vertices[1] = DijkstraVertex.parse("1(0, 0.2)(6, 0.4)(7, 0.3)(9, 0.1)");
+        dijkstra.vertices[2] = DijkstraVertex.parse("2(4, 0.5)");
+        dijkstra.vertices[3] = DijkstraVertex.parse("3(5, 1)");
+        dijkstra.vertices[4] = DijkstraVertex.parse("4(2,0.4)(6,0.6)(7,0.3)");
+        dijkstra.vertices[5] = DijkstraVertex.parse("5(3,1)");
+        dijkstra.vertices[6] = DijkstraVertex.parse("6(4,0.3)");
+        dijkstra.vertices[7] = DijkstraVertex.parse("7(1, 0.3)(4, 0.1)(8, 0.15)(9, 0.05)");
+        dijkstra.vertices[8] = DijkstraVertex.parse("8(0, 0.1)(7, 0.15)");
+        dijkstra.vertices[9] = DijkstraVertex.parse("9(1, 0.1)(2, 0.1)(7, 0.05)");
     }
 
     @Test
-    public void getBestPaTH() {
+    public void parse() {
+        DijkstraVertex v = new DijkstraVertex(0);
+        DijkstraVertex v1 = DijkstraVertex.parse("0");
+        assertEquals(v, v1);
+        v.addConnection(1, 0.5);
+        DijkstraVertex v2 = DijkstraVertex.parse("0(1, 0.5)");
+        assertEquals(v, v2);
+        DijkstraVertex v3 = DijkstraVertex.parse("0(1,0.5)(3,0.3)(9,0.2)(5,0.1)");
+        Map<Integer, Double> newConnections = new HashMap<>();
+        newConnections.put(1, 0.5);
+        newConnections.put(3, 0.3);
+        newConnections.put(9, 0.2);
+        newConnections.put(5, 0.1);
+        v.setConnections(newConnections);
+        assertEquals(v, v3);
+    }
+
+    @Test
+    public void getBestPath() {
+        assertEquals(0.0, dijkstra.getBestPath(1, 1), 0.0001);
+        assertEquals(Double.NEGATIVE_INFINITY, dijkstra.getBestPath(0, 3), 0.0001);
+        assertEquals(Double.NEGATIVE_INFINITY, dijkstra.getBestPath(2, 5), 0.0001);
+        assertEquals(0.3, dijkstra.getBestPath(0, 1), 0.0001);
+        assertEquals(0.2, dijkstra.getBestPath(1, 0), 0.0001);
+        assertEquals(0.25, dijkstra.getBestPath(1, 4), 0.0001);
+        assertEquals(0.45, dijkstra.getBestPath(4, 1), 0.0001);
+        assertEquals(0.35, dijkstra.getBestPath(0, 4), 0.0001);
+        assertEquals(0.55, dijkstra.getBestPath(4, 0), 0.0001);
+        assertEquals(0.3, dijkstra.getBestPath(0, 9), 0.0001);
+        assertEquals(0.3, dijkstra.getBestPath(9, 0), 0.0001);
+        assertEquals(0.2, dijkstra.getBestPath(1, 2), 0.0001);
+        assertEquals(0.95, dijkstra.getBestPath(2, 1), 0.0001);
+        assertEquals(0.65, dijkstra.getBestPath(6, 9), 0.0001);
+        assertEquals(0.5, dijkstra.getBestPath(9, 6), 0.0001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidArguments() {
+        dijkstra.getBestPath(0, dijkstra.size);
     }
 }
