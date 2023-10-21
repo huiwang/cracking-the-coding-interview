61bcdaba421bce75738d4251a3963fa064453723
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-21 10:19:47 +0200

    implement _01_02_CheckPermutation algorithm.

--- src/main/java/arraystring/_01_02_CheckPermutation.java
+++ src/main/java/arraystring/_01_02_CheckPermutation.java
@@ -2,7 +2,21 @@ package arraystring;
 
 /** Given two strings,write a method to decide if one is a permutation of the other. */
 class _01_02_CheckPermutation {
+
+  //Time Complexity O(n) | Space Complexity O(1)
   boolean check(String a, String b) {
-    throw new UnsupportedOperationException();
+
+    var sumCharsA = 0;
+    var sumCharsB = 0;
+
+    for(int i = 0; i < a.length(); i++) {
+      sumCharsA += a.charAt(i);
+    }
+
+    for(int i = 0; i < b.length(); i++) {
+      sumCharsB += b.charAt(i);
+    }
+
+    return sumCharsA == sumCharsB;
   }
 }
