c07e057a03e276188df892a25ec1f9541d201d83
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-21 15:29:01 +0200

    implement _01_04_PalindromePermutation.

--- src/main/java/arraystring/_01_04_PalindromePermutation.java
+++ src/main/java/arraystring/_01_04_PalindromePermutation.java
@@ -1,5 +1,6 @@
 package arraystring;
 
+import java.util.HashMap;
 /**
  * Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is
  * a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of
@@ -8,7 +9,31 @@ package arraystring;
  * <p>EXAMPLE Input: Tact Coa Output: True (permutations: "taco cat", "atco cta", etc.)
  */
 class _01_04_PalindromePermutation {
+
+  //Time complexity O(n) | Space complexity O(n)
   boolean check(String s) {
-    throw new UnsupportedOperationException();
+    if(s.length() == 1) {
+      return true;
+    }
+
+    var map = new HashMap<Character, Integer>();
+
+    for(int i = 0; i < s.length(); i++) {
+      if(!Character.isSpaceChar(s.charAt(i))) {
+        map.merge(s.charAt(i), 1, Integer::sum);
+      }
+    }
+
+    var nonDuplicateNumberCount = 0;
+    for(var entrySet : map.entrySet()) {
+      if(entrySet.getValue() % 2 != 0) {
+        nonDuplicateNumberCount++;
+        if(nonDuplicateNumberCount > 1) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 }
