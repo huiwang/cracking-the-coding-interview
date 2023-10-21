d4e5d10f89f677f36a4e81f6fd2db207bf2c411c
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-21 10:03:08 +0200

    implement _01_01_IsUnique algorithm.

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -1,9 +1,19 @@
 package arraystring;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /** Implement an algorithm to determine if a string has all unique characters. */
 class _01_01_IsUnique {
 
+  //Time complexity O(n) | Space complexity O(n)
   boolean isUnique(String str) {
-    throw new UnsupportedOperationException();
+    Set<Character> charSet = new HashSet<>();
+
+    for(char c : str.toCharArray()) {
+      charSet.add(c);
+    }
+
+    return charSet.size() == str.length();
   }
 }
