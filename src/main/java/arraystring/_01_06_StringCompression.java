7d7f5be4f2b18c00abb738a37ef055addf824aec
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-22 16:21:21 +0200

    implement _01_06_StringCompression and fix testcases;

--- src/main/java/arraystring/_01_06_StringCompression.java
+++ src/main/java/arraystring/_01_06_StringCompression.java
@@ -7,7 +7,38 @@ package arraystring;
  * assume the string has only uppercase and lowercase letters (a - z).
  */
 class _01_06_StringCompression {
+  //Time complexity O(n) | Space complexity O (n)
   String compress(String s) {
-    throw new UnsupportedOperationException();
+    if(s.length() == 1 || s.isEmpty()) {
+      return s;
+    }
+
+    int count = 1;
+    char c = ' ';
+    var sb = new StringBuilder();
+
+    for(int i = 0; i < s.length(); i++) {
+      if(c == ' ') {
+        c = s.charAt(i);
+      }
+      else if(c == s.charAt(i)) {
+        count++;
+      }
+      else {
+        sb.append(c).append(count);
+        count = 1;
+        c = s.charAt(i);
+      }
+
+      if(i == s.length() -1) {
+        sb.append(c).append(count);
+      }
+    }
+
+    if(sb.toString().length() < s.length()) {
+      return sb.toString();
+    } else {
+      return s;
+    }
   }
 }
--- src/test/java/arraystring/_01_06_StringCompressionTest.java
+++ src/test/java/arraystring/_01_06_StringCompressionTest.java
@@ -20,7 +20,7 @@ class _01_06_StringCompressionTest {
 
   @Test
   void withThreeChars() {
-    assertEquals("3a", s.compress("aaa"));
+    assertEquals("a3", s.compress("aaa"));
   }
 
   @Test
@@ -30,11 +30,11 @@ class _01_06_StringCompressionTest {
 
   @Test
   void withDifferentChars_RepeatingSeveralTimes() {
-    assertEquals("3a2b", s.compress("aaabb"));
+    assertEquals("a3b2", s.compress("aaabb"));
   }
 
   @Test
   void withDifferentChars_RepeatingInMoreSeqs() {
-    assertEquals("3a2b2a", s.compress("aaabbaa"));
+    assertEquals("a3b2a2", s.compress("aaabbaa"));
   }
 }
