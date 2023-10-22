6f188c5c615717a0f1094d0cd7fe839b22d11a1d
Alexander Zotz <alexander.zotz.external@eon.de>
2023-10-22 10:41:43 +0200

    implement _01_05_OneAway.

--- src/main/java/arraystring/_01_05_OneAway.java
+++ src/main/java/arraystring/_01_05_OneAway.java
@@ -1,5 +1,7 @@
 package arraystring;
 
+import java.util.HashMap;
+
 /**
  * There are three types of edits that can be performed on strings: insert a character, remove a
  * character, or replace a character. Given two strings, write a function to check if they are one
@@ -8,7 +10,40 @@ package arraystring;
  * <p>EXAMPLE pale, ple -> true pales, pale -> true pale, bale -> true pale, bake -> false
  */
 class _01_05_OneAway {
+
+  //Time complexity O(n) | Space complexity O(n)
   boolean isOneAway(String a, String b) {
-    throw new UnsupportedOperationException();
+    if(a.isEmpty() && b.isEmpty()) {
+      return true;
+    }
+
+    if(Math.abs(a.length() - b.length()) > 1) {
+      return false;
+    }
+
+    int away = 0;
+
+    var lookupA = new HashMap<Character, Integer>();
+    for(int i = 0; i < a.length(); i++) {
+      lookupA.merge(a.charAt(i), 1, Integer::sum);
+    }
+
+    var lookupB = new HashMap<Character, Integer>();
+    for(int i = 0; i < b.length(); i++) {
+      lookupB.merge(b.charAt(i), 1, Integer::sum);
+    }
+
+    for(var entrySet : lookupB.entrySet()) {
+      if(!lookupA.containsKey(entrySet.getKey())) {
+        away += entrySet.getValue();
+      } else {
+        away += Math.abs(entrySet.getValue() - lookupA.get(entrySet.getKey()));
+      }
+
+      if(away > 1) {
+        return false;
+      }
+    }
+    return true;
   }
 }
